d552bbcfb7a01f234068565684717a1ae2bb8753
vadik <etoseryozno@yandex.ru>
2022-11-19 17:25:00 +0300

    added django-dotenv

---
+++ stripe_example/config/wsgi.py
@@ -0,0 +1,15 @@
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+import dotenv
+
+dotenv.read_dotenv(
+    os.path.join(
+        os.path.dirname(os.path.dirname(__file__)), '.env'
+    )
+)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stripe_example.settings')
+
+application = get_wsgi_application()
